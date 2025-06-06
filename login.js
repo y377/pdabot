02d319c9aadbed6d50a248add192bb38efec4dba
y377 <58632405+y377@users.noreply.github.com>
2025-06-07 00:38:06 +0800

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@
 
 // 1. 配置和全局变量
 const FEISHU_CONFIG = {
-  client_id: 'cli_a8b10d009ef8d00c',
+  client_id: 'cli_a8bfe46f2e6e500b',
   redirect_uri: 'https://pdabot.jsjs.net/'
 };
 let currentUser = null;
